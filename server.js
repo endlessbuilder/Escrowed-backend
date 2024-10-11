0df75ae078b368eee8bd9112aa7d7a57288db804
endlessbuilder <continuebuilding@gmail.com>
2024-10-11 06:36:10 -0400

    add: cors policy

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const { sequelize } = require('./models');
-const userRoutes = require('./routes/user');
-const deedRoutes= require("./routes/deed");
+const express = require("express");
+const { sequelize } = require("./models");
+const userRoutes = require("./routes/user");
+const deedRoutes = require("./routes/deed");
 const disputeRoutes = require("./routes/dispute");
 const logRoutes = require("./routes/log");
 const workRoutes = require("./routes/work");
-const faqRoutes = require('./routes/faq');
-const suggestionRoutes = require('./routes/suggestion'); 
+const faqRoutes = require("./routes/faq");
+const suggestionRoutes = require("./routes/suggestion");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
-app.use('/user', userRoutes);
-app.use('/deed', deedRoutes);
-app.use('/dispute', disputeRoutes);
-app.use('/log', logRoutes);
-app.use('/work', workRoutes);
-app.use('/faq', faqRoutes); 
-app.use('/suggestion', suggestionRoutes);
+app.use("/user", userRoutes);
+app.use("/deed", deedRoutes);
+app.use("/dispute", disputeRoutes);
+app.use("/log", logRoutes);
+app.use("/work", workRoutes);
+app.use("/faq", faqRoutes);
+app.use("/suggestion", suggestionRoutes);
 
 
-app.get('/', (req, res) => {
-  res.send('Escrow Backend Server');
+app.get("/", (req, res) => {
+  res.send("Escrow Backend Server");
 });
 
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
   });
-}).catch((error) => {
-  console.error('Unable to connect to the database:', error);
-});
